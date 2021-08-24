f7cd30b9167ff3f808760197056eb8a7b1845c1c
Abhishek karn <proabhishek732@gmail.com>
2021-08-24 09:26:57 +0545

    few changes in the UI

--- app/src/main/java/com/example/unitconverter/SecondActivity.kt
+++ app/src/main/java/com/example/unitconverter/SecondActivity.kt
@@ -1,6 +1,5 @@
 package com.example.unitconverter
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unitconverter.databinding.SecondActivityBinding
@@ -13,20 +12,13 @@ class SecondActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.backButton.setOnClickListener {
-            Intent(this, MainActivity::class.java).also {
-                startActivity(it)
-            }
+
         }
 
-        binding.convertButton.setOnClickListener {
-            conversion()
-        }
 
     }
-    private fun conversion(){
-        val userInput = binding.edittext.text.toString().toInt()
-        val outputValue = userInput * 100
-        binding.textView.text = outputValue.toString()
-    }
-}
+//    private fun conversion(){
+//        val userInput = binding.edittext.text.toString().toInt()
+//        val outputValue = userInput * 100
+//        binding.textView.text = outputValue.toString()
+//    }
